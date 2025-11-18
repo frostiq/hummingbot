4dda061d87ae529baa8231a741ccfdb032f414d9
Alex Bazhanau <alex@bazhanau.net>
2025-11-18 00:03:51 -0400

    Add error logging for unsuccessful WebSocket messages in BybitPerpetualAPIOrderBookDataSource

--- hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_api_order_book_data_source.py
+++ hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_api_order_book_data_source.py
@@ -189,6 +189,9 @@ class BybitPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
                 channel = self._diff_messages_queue_key
             elif event_channel == CONSTANTS.WS_INSTRUMENTS_INFO_TOPIC:
                 channel = self._funding_info_messages_queue_key
+        else:
+            if event_message["success"] is False:
+                self.logger().error(f"Error message received from Bybit WebSocket: {event_message}")
         return channel
 
     async def _parse_order_book_diff_message(self, raw_message: Dict[str, Any], message_queue: asyncio.Queue):
